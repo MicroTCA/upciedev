e2eb7f4053a18488e3a9f55c47b12296b91c4737
Ludwig Petrosyan <ludwig.petrosyan@desy.de>
2017-12-08 10:10:25 +0100

    Add copyright and license notices

--- pciedev_io.h
+++ pciedev_io.h
@@ -1,3 +1,26 @@
+/**
+*Copyright 2016-  DESY (Deutsches Elektronen-Synchrotron, www.desy.de)
+*
+*This file is part of UPCIEDEV driver.
+*
+*Foobar is free software: you can redistribute it and/or modify
+*it under the terms of the GNU General Public License as published by
+*the Free Software Foundation, either version 3 of the License, or
+*(at your option) any later version.
+*
+*Foobar is distributed in the hope that it will be useful,
+*but WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*GNU General Public License for more details.
+*
+*You should have received a copy of the GNU General Public License
+*along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+/*
+*	Author: Ludwig Petrosyan (Email: ludwig.petrosyan@desy.de)
+*/
+
 #ifndef _PCIEDEV_IO_H
 #define _PCIEDEV_IO_H
 
--- pciedev_remove_exp.c
+++ pciedev_remove_exp.c
@@ -1,3 +1,27 @@
+/**
+*Copyright 2016-  DESY (Deutsches Elektronen-Synchrotron, www.desy.de)
+*
+*This file is part of UPCIEDEV driver.
+*
+*Foobar is free software: you can redistribute it and/or modify
+*it under the terms of the GNU General Public License as published by
+*the Free Software Foundation, either version 3 of the License, or
+*(at your option) any later version.
+*
+*Foobar is distributed in the hope that it will be useful,
+*but WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*GNU General Public License for more details.
+*
+*You should have received a copy of the GNU General Public License
+*along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+/*
+*	Author: Ludwig Petrosyan (Email: ludwig.petrosyan@desy.de)
+*/
+
+
 #include <linux/module.h>
 #include <linux/fs.h>	
 #include <linux/proc_fs.h>
